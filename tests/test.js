cd4799e8614afa440a9c19b42d6c1f8fe41b054d
Said-laasri <saidlaasri3@gmail.com>
2023-07-03 14:57:51 +0100

    test all pass

--- tests/test.js
+++ tests/test.js
@@ -22,24 +22,41 @@ describe("People", () => {
         });
     });
     it("should create a people entry with valid input", (done) => {
-      // your code goes here
-      done();
+      chai
+        .request(app)
+        .post("/api/v1/people")
+        .send({ name: "John", age: 10 })
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.have
+            .property("message")
+            .equal("A person record was added");
+          res.body.should.have
+            .property("people")
+            .to.be.an("array")
+            .that.deep.includes({ age: 10, name: "John" });
+
+          done();
+        });
     });
-  });
-  describe("get /api/v1/people", () => {
-    it(`should return an array of person entries of length ${this.lastIndex + 1}`, (done) => {
-           // your code goes here
-           done();
+
+    describe("get /api/v1/people", () => {
+      it(`should return an array of person entries of length ${
+        this.lastIndex + 1
+      }`, (done) => {
+        // your code goes here
+        done();
+      });
     });
-  });
-  describe("get /apl/v1/people/:id", () => {
-    it("should return the entry corresponding to the last person added.", (done) => {
-      // your code goes here
-      done();
-    });
-    it("should return an error if the index is >= the length of the array", (done) => {
-            // your code goes here
-            done();
+    describe("get /apl/v1/people/:id", () => {
+      it("should return the entry corresponding to the last person added.", (done) => {
+        // your code goes here
+        done();
+      });
+      it("should return an error if the index is >= the length of the array", (done) => {
+        // your code goes here
+        done();
+      });
     });
   });
 });
